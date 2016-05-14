dffdd37e94e3c815dd5e0579175268845169e211
LubchenkoGleb <gleb.lubchenko@gmail.com>
2016-05-14 13:15:40 -0700

    classes added

---
+++ app/src/main/java/com/example/sandbox/SharingFile.java
@@ -0,0 +1,13 @@
+package com.example.sandbox;
+
+import android.graphics.Path;
+
+/**
+ * Created by glebl_000 on 5/14/2016.
+ */
+public class SharingFile {
+    private static int idCounter = 0;
+    private int id;
+    private static Path fileLocation;
+
+}
---
+++ app/src/main/java/com/example/sandbox/Task.java
@@ -0,0 +1,9 @@
+package com.example.sandbox;
+
+/**
+ * Created by glebl_000 on 5/14/2016.
+ */
+public class Task {
+    private int id;
+    private enum request{SEND, GET};
+}
